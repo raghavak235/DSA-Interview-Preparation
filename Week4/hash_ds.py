1682c9576e1f35b71877cc907427e4b02fd2670d
raghavarahul <raghavak235@gmail.com>
2024-08-04 14:34:25 -0400

    Adding Hashing

---
+++ Week4/hash_ds.py
@@ -0,0 +1,14 @@
+# Time Complexity:O(n)
+# Space Complexity:O(n)
+def count_char(string):
+    char_count={}
+    for i in string:
+        if i in char_count:
+            char_count[i] += 1
+        else:
+            char_count[i]=1
+
+    print(char_count)
+
+
+count_char('abcdd')
