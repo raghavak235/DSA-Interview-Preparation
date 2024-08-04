95f3259c960c1b4241e1e3a89999a61943707b8f
raghavarahul <raghavak235@gmail.com>
2024-08-03 22:42:55 -0400

    Adding LL Reverse

---
+++ Week4/linkedList_reverse.py
@@ -0,0 +1,24 @@
+# ime Complexity:O(n)
+# Space Complexity: O(1)
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        prev = None
+        curr = head
+        next_ptr = None
+
+        while curr != None:
+            next_ptr = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next_ptr
+        head = prev
+        return head
