47ecd32c6092aa6eac84d15e47256fde283007f4
raghavarahul <raghavak235@gmail.com>
2024-08-06 10:23:01 -0400

    Updating Hashing

--- Week4/HashOperations.py
+++ Week4/HashOperations.py
@@ -1,22 +1,27 @@
 
-    class HashTable:
-        def __init__(self):
-            self.max= 10
-            self.arr=[None for i in range(self.max)]
+class HashTable:
+    def __init__(self):
+        self.max= 10
+        self.arr=[None for i in range(self.max)]
 
-        def hash_function(self, key):
-            summation = 0
-            m=10
-            for char in key:
-                summation += ord(char)
+    def hash_function(self, key):
+        summation = 0
+        m=10
+        for char in key:
+            summation += ord(char)
 
-            return summation%m
+        return summation%m
 
-        def addItem(self, key, value):
-            h = self.hash_function(key)
-            self.arr[h]=value
+    def addItem(self, key, value):
+        h = self.hash_function(key)
+        self.arr[h]=value
 
 
-        def getItem(self, key):
-            h = self.hash_function(key)
-            return self.arr[h]
+    def getItem(self, key):
+        h = self.hash_function(key)
+        return self.arr[h]
+
+
+has_obj=HashTable()
+has_obj.addItem('March 2024',123)
+has_obj.getItem('March 2024')
