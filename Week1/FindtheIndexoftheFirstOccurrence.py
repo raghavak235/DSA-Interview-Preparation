e2d7514b7d6c215ba00c6c4a2b9be7c019999df8
raghavarahul <raghavak235@gmail.com>
2024-10-15 18:14:38 +0530

    Adding FindtheIndexoftheFirstOccurrence

---
+++ Week1/FindtheIndexoftheFirstOccurrence.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/description/?envType=study-plan-v2&envId=top-interview-150
+
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        return haystack.find(needle)
